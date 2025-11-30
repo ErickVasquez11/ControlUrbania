13a33eb7d76cb307bef258cf22391acfd446c9e0
ErickVasquez11 <00065520@uca.edu.sv>
2025-11-30 11:26:16 -0600

    :construction: Connection

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL="https://uzdxoibnuwdasevnpska.supabase.co";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6ZHhvaWJudXdkYXNldm5wc2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMTAwMTQsImV4cCI6MjA3ODY4NjAxNH0.7j0c8WzjHrZ0t0qHZI1uURclXiC3bhpWFGdfcwEYew8";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
